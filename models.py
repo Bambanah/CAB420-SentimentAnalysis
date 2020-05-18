24aecae6b3097531070cc53855fdfeca83ad6531
Lachie <lachlanu@gmail.com>
2020-05-19 02:29:24 +1000

    Redesigned LSTM network

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 # Disable tensorflow debugging information
 
-
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation
 from tensorflow.keras.layers import Embedding
-from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import LSTM, GRU
 from tensorflow.keras.layers import Conv1D, MaxPooling1D
+
 
 
 def lstm(vocab_size,
@@ -21,7 +22,7 @@ def lstm(vocab_size,
 
     model.add(Embedding(vocab_size, embedding_size, input_length=maxlen))
     model.add(Dropout(0.2))
-
+    #
     model.add(Conv1D(filters,
                      kernel_size,
                      padding='valid',
@@ -29,13 +30,23 @@ def lstm(vocab_size,
                      strides=1))
     model.add(MaxPooling1D(pool_size=pool_size))
 
-    model.add(LSTM(lstm_output_size))
+    model.add(LSTM(lstm_output_size, return_sequences=True))
+    model.add(Dropout(0.2))
 
-    model.add(Dense(1))
-    model.add(Activation('sigmoid'))
+    model.add(LSTM(lstm_output_size))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(1, activation='sigmoid'))
+
+    opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
 
     model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
+                  optimizer=opt,
                   metrics=metrics)
+
+    print(model.summary())
 
     return model
